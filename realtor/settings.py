b9c8080266bb3fb219f9e8351f5f6a743c91cb75
Leon Kleinhans <hello@leonkleinhans.com>
2021-06-11 01:52:51 +0200

    postgres db, models, migrate

--- realtor/settings.py
+++ realtor/settings.py
@@ -79,8 +79,12 @@ WSGI_APPLICATION = 'realtor.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'realtordb',  # 'NAME': BASE_DIR / 'db.sqlite3',
+        'USER': 'postgres',
+        'PASSWORD': 'p05tgr35',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
