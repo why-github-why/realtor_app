1e95007e222dc1abe10fac4db7df3de0d39126d8
Leon Kleinhans <hello@leonkleinhans.com>
2021-06-12 01:27:07 +0200

    admin, all listings

--- realtor/settings.py
+++ realtor/settings.py
@@ -135,3 +135,7 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# MEDIA FOLDER SETTINGS
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
