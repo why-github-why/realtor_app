7c8685c26272fa4487185061dbd7d9982769cbce
Leon Kleinhans <hello@leonkleinhans.com>
2021-06-12 21:00:09 +0200

    dynamic listings

--- listings/views.py
+++ listings/views.py
@@ -1,11 +1,18 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Listing
 
-# Create your views here.
 def index(request):
-   return render(request, 'listings/listings.html')
+   # pass postgresql database as object
+   listings = Listing.objects.all()
 
-def listing(request):
+   context = {
+      'listings': listings,
+   }
+
+   return render(request, 'listings/listings.html', context)
+
+def listing(request, listing_id):
    return render(request, 'listings/listing.html')
 
 def search(request):
