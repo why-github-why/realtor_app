28f68d715f9d4f0d2fcf080040f046222c29df12
Leon Kleinhans <hello@leonkleinhans.com>
2021-06-13 21:35:25 +0200

    realtors, mvp realtor(s)

--- pages/views.py
+++ pages/views.py
@@ -1,9 +1,24 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from listings.models import Listing
+from realtors.models import Realtor
 
-# Create your views here.
 def index(request):
-   return render(request, 'pages/index.html')
+   # [:3] - Display only 3 listings
+   listings = Listing.objects.order_by('-list_date').filter(is_published=True)[:3]
+
+   return render(request, 'pages/index.html', {'listings': listings})
 
 def about(request):
-   return render(request, 'pages/about.html')
+   # Get all realtors
+   realtors = Realtor.objects.order_by('hire_date')
+
+   # Get MVP realtor(s)
+   mvp_realtors = Realtor.objects.all().filter(is_mvp=True)
+
+   context = {
+      'realtors': realtors,
+      'mvp_realtors': mvp_realtors,
+   }
+
+   return render(request, 'pages/about.html', context)
