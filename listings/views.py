d30788b1dcc6d4334be7c528cdc4d66351ad9b12
Leon Kleinhans <hello@leonkleinhans.com>
2021-06-16 21:17:47 +0200

    search query

--- listings/views.py
+++ listings/views.py
@@ -28,10 +28,48 @@ def listing(request, listing_id):
    return render(request, 'listings/listing.html', context)
 
 def search(request):
+   search_query = Listing.objects.order_by('-list_date')
+
+   # Keyword
+   if 'keywords' in request.GET:
+      # request.GET[name attribute in html]
+      keywords = request.GET['keywords']
+      # check if keywords exist
+      if keywords:
+         # icontains - check if paragraph (description) contains the keyword
+         search_query = search_query.filter(description__icontains=keywords)
+
+   # City
+   if 'city' in request.GET:
+      city = request.GET['city']
+      if city:
+         # iexact - exact city but case-insensitive
+         search_query = search_query.filter(city__iexact=city)
+
+   # State
+   if 'state' in request.GET:
+      state = request.GET['state']
+      if state:
+         search_query = search_query.filter(state__iexact=state)
+
+   # Bedrooms
+   if 'bedrooms' in request.GET:
+      bedrooms = request.GET['bedrooms']
+      if bedrooms:
+         search_query = search_query.filter(bedrooms__iexact=bedrooms)  
+
+   # Max Price
+   if 'price' in request.GET:
+      price = request.GET['price']
+      if price:
+         # lte - less than or equal to (up to queried price)
+         search_query = search_query.filter(price__lte=price)
+
    context = {
       'state_choices': state_choices,
       'bedroom_choices': bedroom_choices,
       'price_choices': price_choices,
+      'listings': search_query,
    }
 
    return render(request, 'listings/search.html', context)
