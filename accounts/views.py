2c1f472b0cc72c393dd8b310dde67f8165cc2c65
Leon Kleinhans <hello@leonkleinhans.com>
2021-06-22 21:29:39 +0200

    logout, dashboard

--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,6 @@
 from django.contrib import messages
 from django.shortcuts import render, redirect
-from django.contrib import messages
+from django.contrib import messages, auth
 from django.contrib.auth.models import User
 
 def register(request):
@@ -48,13 +48,31 @@ def register(request):
 
 def login(request):
    if request.method == 'POST':
-   # login user
-      return
+      # login user
+      username = request.POST['username']
+      password = request.POST['password']
 
-   return render(request, 'accounts/login.html')
+      # authenticate user
+      user = auth.authenticate(username=username, password=password)
+
+      # check if user exists in database
+      if user is not None:
+         auth.login(request, user)
+         messages.success(request, 'You are now logged in.')
+         return redirect('dashboard')
+      else:
+         messages.error(request, 'Invalid credentials.')
+         return redirect('login')
+
+   else:
+      return render(request, 'accounts/login.html')
 
 def logout(request):
-   return redirect('index')
+   if request.method == 'POST':
+      # logout user
+      auth.logout(request)
+      messages.success(request, 'You are now logged out.')
+      return redirect('index')
 
 def dashboard(request):
    return render(request, 'accounts/dashboard.html')
--- realtor/settings.py
+++ realtor/settings.py
@@ -124,7 +124,7 @@ USE_L10N = True
 USE_TZ = True
 
 
-# Static files (CSS, JavaScript, Images)
+# STATIC FILES (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
