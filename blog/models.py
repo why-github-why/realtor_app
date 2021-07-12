dd7543fac4d6fbd05976943c94cc584c1c9562b9
Leon Kleinhans <hello@leonkleinhans.com>
2021-07-12 21:10:55 +0200

    subject_date

--- blog/models.py
+++ blog/models.py
@@ -27,7 +27,7 @@ class Post(models.Model):
    image_2 = models.ImageField(upload_to='photos/%Y/%m/%d/', blank=True)
    image_3 = models.ImageField(upload_to='photos/%Y/%m/%d/', blank=True)
    image_4 = models.ImageField(upload_to='photos/%Y/%m/%d/', blank=True)
-   subject_date = models.DateTimeField(default=datetime.now(), blank=True)
+   subject_date = models.DateTimeField(default=datetime.now, blank=True)
 
    def __str__(self):
       return f"{self.subject} | {self.author}"
